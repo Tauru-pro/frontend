4f01d626d86de9a6792984256eac4d435e0c3d55
Deimer Hernandez <dhernandez@croper.com>
2026-05-14 16:19:09 -0500

    fix: resolve routes to prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: 'catalog/:id', renderMode: RenderMode.Server },
+  { path: 'admin/users/:id/edit', renderMode: RenderMode.Server },
+  { path: 'admin/pickup-points/:id/edit', renderMode: RenderMode.Server },
+  { path: 'admin/shipping-rates/:id/edit', renderMode: RenderMode.Server },
+  { path: 'admin/breeds/:id/edit', renderMode: RenderMode.Server },
+  { path: 'seller/products/:id/edit', renderMode: RenderMode.Server },
+  { path: 'seller/bulls/:id/edit', renderMode: RenderMode.Server },
+  { path: '**', renderMode: RenderMode.Server },
 ];
